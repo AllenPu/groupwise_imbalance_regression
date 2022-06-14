75e9f7c086aeae791da96d6a5dab42a41c0d0d37
AllenPu <18651885620@163.com>
2022-06-14 19:46:34 -0400

    .\loss.py

---
+++ loss.py
@@ -0,0 +1,42 @@
+class LAloss(nn.Module):
+    def __init__(self, cls_num_list, tau=1.0):
+        super(LAloss, self).__init__()
+
+        cls_probs = [cls_num / sum(cls_num_list) for cls_num in cls_num_list]
+        iota_list = tau * np.log(cls_probs)
+
+        self.iota_list = torch.cuda.FloatTensor(iota_list)
+
+    def forward(self, x, target):
+        output = x + self.iota_list
+
+        return F.cross_entropy(output, target)
+
+
+
+def forward(self, x, y, g):
+    # x shape is N, W,H,C
+    # y shape is N,1
+    # g shape is N,1
+    "output of model dim is 2G"
+    y_hat = self.model(x)
+    output_len = len(y_hat)
+    #first G is cls
+    g_hat = y_hat[: output_len/2]
+    # add one to the cls num
+    self.cls_num_list[g] += 1
+    # cross entropy
+    loss_la = LAloss(self.cls_num_list, tau=1.0)
+    # compute the group cls loss
+    loss_ce = loss_la(g_hat, g)
+    # add MSE
+    if self.mode == 'train':
+        y_hat_index = output_len/2 + g
+        y_hat = output[y_hat_index]
+    else:
+        y_hat_index = output_len/2 + torch.argmax(g_hat, dim =1)
+        y_hat = output[y_hat_index]
+    loss_mse = nn.MSELoss(y_hat, y)
+    # total loss
+    loss = loss_ce + loss_mse
+    return y_hat, g_hat, loss
