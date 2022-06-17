d1090ac1712fcc2b9f4ba50896f68f4af09acc96
AllenPu <18651885620@163.com>
2022-06-17 02:30:18 -0400

    update

---
+++ network.py
@@ -0,0 +1,62 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from loss import LAloss
+
+
+class ResNet_regression(nn.Module):
+    def __init__(self, args):
+        super(ResNet_regression, self).__init__()
+        self.model = torchvision.models.resnet18(pretrained=False)
+        output_dim = args.num_groups*2
+        fc_inputs = self.model.fc.in_features
+        self.model.fc = nn.Sequential(
+            nn.Linear(fc_inputs, 1024),
+            #nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(1024, output_dim)
+        )
+        ###### LOSS
+        #self.loss_mse = torch.nn.MSELoss()
+        #self.loss_la = LAloss(self.cls_num_list, tau=1.0)
+        ######
+        self.mode = args.mode
+        self.sigma = args.sigma
+        self.output_strategy = args.outpt_strategy
+        self.weighted = args.weighted
+        
+    # g is the same shape of y
+    def forward(self, x, g):
+        #"output of model dim is 2G"
+        y_hat = self.model(x)
+        output_len = len(y_hat)
+        #first G is cls
+        g_hat = y_hat[: , : output_len/2]
+        # add MSE
+        ''' 
+        to be added into the model ini part
+        '''
+        if self.mode == 'train':
+        #loss_la = LAloss(self.cls_num_list, tau=1.0)
+        # compute the group cls loss
+            #loss_ce = self.loss_la(g_hat, g)
+            g_len = output_len/2
+            g_index = g.unsqueeze(-1) + g_len
+            yhat = torch.gather(y_hat, dim = 1, index = g_index).squeeze(-1)
+            #loss_mse = self.loss_mse(yhat, y)
+            #loss = loss_mse + self.sigma *loss_ce
+        else:
+            if self.output_strategy == 1:
+                y_hat_index = output_len/2 + torch.argmax(g_hat, dim =1).unsqueeze(-1)
+                yhat = torch.gather(y_hat, dim = 1, index = y_hat_index).squeeze(-1)
+            elif self.output_strategy == 2:
+                # wegihted
+                #yhat = torch.sum(self.weighted * y_hat[output_len/2:])
+                pass
+            else:
+                assert "no output strategy defined"
+        return yhat, g_hat
