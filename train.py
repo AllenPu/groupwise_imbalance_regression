7796338f40365c50780391873c2b6d1b5bec5c3a
AllenPu <18651885620@163.com>
2022-07-16 17:55:51 -0400

    update

--- train.py
+++ train.py
@@ -41,6 +41,7 @@ parser.add_argument('--workers', type=int, default=32, help='number of workers u
 parser.add_argument('--lr', type=float, default=1e-3, help='initial learning rate')
 parser.add_argument('--seeds', default=123, type=int, help = ' random seed ')
 parser.add_argument('--tau', default=1, type=int, help = ' tau for logit adjustment ')
+parser.add_argument('--group_mode', default='normal', type=str, help = ' group mode for group orgnize')
 
 
 def get_dataset(args):
@@ -48,6 +49,10 @@ def get_dataset(args):
     print(f"File (.csv): {args.dataset}.csv")
     df = pd.read_csv(os.path.join(args.data_dir, f"{args.dataset}.csv"))
     df_train, df_val, df_test = df[df['split'] == 'train'], df[df['split'] == 'val'], df[df['split'] == 'test']
+    ##### how to orgnize the datastes
+    if args.group_mode != 'normal':
+        nb_groups = int(args.groups)
+        df_train = group_df(df_train, nb_groups)
     train_dataset = IMDBWIKI(data_dir=args.data_dir, df=df_train, img_size=args.img_size, split='train', group_num = args.groups)
     val_dataset = IMDBWIKI(data_dir=args.data_dir, df=df_val, img_size=args.img_size, split='val', group_num = args.groups)
     test_dataset = IMDBWIKI(data_dir=args.data_dir, df=df_test, img_size=args.img_size, split='test', group_num = args.groups)
