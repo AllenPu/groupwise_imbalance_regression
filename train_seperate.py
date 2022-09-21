cb52fd62e472e036e174bebb18609c29d586ca4b
AllenPu <18651885620@163.com>
2022-09-20 20:42:28 -0400

    add sepearte test

---
+++ train_seperate.py
@@ -0,0 +1,209 @@
+import argparse
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+import random
+import numpy as np
+import json
+import os
+import torch
+import sys
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.models as models
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+from PIL import Image
+import argparse
+import time
+import math
+import pandas as pd
+from loss import LAloss
+from network import ResNet_regression, ResNet_ordinal_regression
+from datasets.IMDBWIKI import IMDBWIKI
+from utils import AverageMeter, accuracy, adjust_learning_rate
+from datasets.datasets_utils import group_df
+import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f" training on ", device)
+parser = argparse.ArgumentParser('argument for training')
+parser.add_argument('--seed', default=123)
+parser.add_argument('--mode', default='train', type= str)
+parser.add_argument('--sigma', default=1.0, type=float)
+parser.add_argument('--epoch', default=100, type=int)
+parser.add_argument('--dataset', type=str, default='imdb_wiki', choices=['imdb_wiki'], help='dataset name')
+parser.add_argument('--data_dir', type=str, default='./data', help='data directory')
+parser.add_argument('--img_size', type=int, default=224, help='image size used in training')
+parser.add_argument('--groups', type=int, default=10, help='number of split bins to the wole datasets')
+parser.add_argument('--batch_size', type=int, default=256, help='batch size')
+parser.add_argument('--workers', type=int, default=32, help='number of workers used in data loading')
+parser.add_argument('--lr', type=float, default=1e-3, help='initial learning rate')
+parser.add_argument('--seeds', default=123, type=int, help = ' random seed ')
+parser.add_argument('--tau', default=1, type=int, help = ' tau for logit adjustment ')
+parser.add_argument('--group_mode', default='normal', type=str, help = ' group mode for group orgnize')
+parser.add_argument('--schedule', type=int, nargs='*', default=[60, 80], help='lr schedule (when to drop lr by 10x)')
+
+
+def get_dataset(args):
+    print('=====> Preparing data...')
+    print(f"File (.csv): {args.dataset}.csv")
+    df = pd.read_csv(os.path.join(args.data_dir, f"{args.dataset}.csv"))
+    if args.group_mode != 'normal':
+        nb_groups = int(args.groups)
+        df = group_df(df, nb_groups)
+    df_train, df_val, df_test = df[df['split'] == 'train'], df[df['split'] == 'val'], df[df['split'] == 'test']
+
+    group = args.groups
+    train_list = []
+    test_list = []
+    val_list = []
+    train_cls_num = []
+    group_range = int(100/group)
+    for i in range(group):
+        start = i * group_range
+        end = (i+1) * group_range
+        if i == group - 1:
+            df_train = df_train[ start<= df_train['age']]
+            df_test = df_test[ start<= df_test['age']]
+            df_val = df_val[ start<= df_val['age']]
+        else:
+            df_train = df_train[ start<= df_train['age'] < end]
+            df_test = df_test[ start<= df_test['age'] < end]
+            df_val = df_val[ start<= df_val['age'] < end]
+        train_dataset = IMDBWIKI(data_dir=args.data_dir, df=df_train, img_size=args.img_size, split='train', group_num = args.groups)
+        val_dataset = IMDBWIKI(data_dir=args.data_dir, df=df_val, img_size=args.img_size, split='val', group_num = args.groups)
+        test_dataset = IMDBWIKI(data_dir=args.data_dir, df=df_test, img_size=args.img_size, split='test', group_num = args.groups)
+        #
+        train_group_cls_num = train_dataset.get_group()
+        #
+        train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
+                              num_workers=args.workers, pin_memory=True, drop_last=False)
+        val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
+                            num_workers=args.workers, pin_memory=True, drop_last=False)
+        test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
+                             num_workers=args.workers, pin_memory=True, drop_last=False)
+        print(f"Group dataset is : {i}")
+        print(f"Training data size: {len(train_dataset)}")
+        print(f"Validation data size: {len(val_dataset)}")
+        print(f"Test data size: {len(test_dataset)}")
+        #
+        train_list.append(train_loader)
+        test_list.append(test_loader)
+        val_list.append(val_loader)
+        train_cls_num.append(train_group_cls_num)
+        #
+    return train_list, test_list, val_list, train_cls_num
+
+
+def train_one_epoch(model, train_loader, mse_loss, opt, device, sigma):
+    model.train()
+    for idx, (x, y, g) in enumerate(train_loader):
+        opt.zero_grad()
+        # x shape : (batch,channel, H, W)
+        # y shape : (batch, 1)
+        # g hsape : (batch, 1)
+        x, y, g = x.to(device), y.to(device), g.to(device)
+        #
+        y_output, z = model(x)
+
+        #split into two parts : first is the group, second is the prediction
+        y_chunk = torch.chunk(y_output, 2, dim = 1)
+        g_hat, y_hat = y_chunk[0], y_chunk[1]
+        #
+        #extract y out
+        y_predicted = torch.gather(y_hat, dim = 1, index = g.to(torch.int64))
+        #
+        mse_y = mse_loss(y_predicted, y)
+        ce_g = F.cross_entropy(g_hat, g.squeeze().long())
+        #
+        loss = mse_y + sigma*ce_g
+        loss.backward()
+        opt.step()
+        #
+    return model
+
+def test_step(model, test_loader, device):
+    model.eval()
+    mse_pred = AverageMeter()
+    mse_mean = AverageMeter()
+    acc_g = AverageMeter()
+    acc_mae = AverageMeter()
+    mse = nn.MSELoss()
+    for idx, (inputs, targets, group) in enumerate(test_loader):
+        #
+        bsz = targets.shape[0]
+        #
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        group = group.to(device)
+
+        with torch.no_grad():
+            y_output, _ = model(inputs.to(torch.float32))
+            y_chunk = torch.chunk(y_output, 2, dim = 1)
+            g_hat, y_hat = y_chunk[0], y_chunk[1]
+            #
+            g_index = torch.argmax(g_hat, dim=1).unsquezze(-1)
+            #
+            group = group.to(torch.int64)
+            #
+            for i in range(group.shape[0]):
+                if group[i].item != g_index[i].item():
+                    print(" orignal is ",g_index[i].item(), " predicted is ",group[i].item)
+            #
+            y_predicted = torch.gather(y_hat, dim = 1, index = group.to(torch.int64))
+            y_predicted_mean = torch.mean(y_hat, dim = 1).unsqueeze(-1)
+            #
+            mse_1 = mse(y_predicted, targets)
+            mse_mean_1 = mse(y_predicted_mean, targets)
+            #
+            reduct = torch.abs(y_predicted - targets)
+            mae_loss = torch.mean(reduct)
+  
+
+            #acc1 = accuracy(y_predicted, targets, topk=(1,))
+            #acc2 = accuracy(y_predicted_mean, targets, topk=(1,))
+            acc3 = accuracy(g_hat, group, topk=(1,))
+
+
+        mse_pred.update(mse_1.item(), bsz)
+        mse_mean.update(mse_mean_1.item(), bsz)
+        acc_g.update(acc3[0].item(), bsz)
+        acc_mae.update(mae_loss.item(), bsz)
+
+    return mse_pred.avg,  mse_mean.avg, acc_g.avg, acc_mae.avg
+
+        
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    random.seed(args.seeds)
+    torch.manual_seed(args.seeds)
+    ####
+    train_loader, test_loader, val_loader,  cls_num_list = get_dataset(args)
+    #
+    loss_mse = nn.MSELoss()
+    loss_ce = LAloss(cls_num_list, tau=args.tau).to(device)
+    #oss_or = nn.MSELoss()
+    #
+    model = ResNet_regression(args).to(device)
+    #model = ResNet_ordinal_regression(args).to(device)
+    # for cls for group only
+    #
+    opt = optim.Adam(model.parameters(), lr=args.lr, weight_decay=5e-4)
+    #
+    sigma = args.sigma
+    #print(" raw model for group classification trained at epoch {}".format(e))
+    for e in tqdm(range(args.epoch)):
+        print(" Training on the epoch ", e)
+        adjust_learning_rate(opt, e, args)
+        model = train_one_epoch(model, train_loader, loss_mse, opt, device, sigma)
+    torch.save(model.state_dict(), './model.pth')
+    acc_y, acc_y2, acc_g, acc_mae = test_step(model, test_loader,device)
+    print(' acc of the max is {}, acc of the mean is {}, acc of the group assinment is {}, mae is {}'.format(acc_y, acc_y2, acc_g, acc_mae))
+    # cls for groups only
+
+     
+            
