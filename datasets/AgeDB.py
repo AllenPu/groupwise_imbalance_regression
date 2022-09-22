45c7b0e917b2ac04eb6bff63c760178d82cd565f
ruizhipu <18651885620@163.com>
2022-09-21 23:01:17 -0400

    agedb

---
+++ datasets/AgeDB.py
@@ -0,0 +1,22 @@
+import os
+import logging
+import numpy as np
+from PIL import Image
+from scipy.ndimage import convolve1d
+from torch.utils import data
+import torchvision.transforms as transforms
+
+
+class AgeDB(data.Dataset):
+    def __init__(self, df, data_dir, img_size, split='train', reweight='none',
+                 lds=False, lds_kernel='gaussian', lds_ks=5, lds_sigma=2):
+        self.df = df
+        self.data_dir = data_dir
+        self.img_size = img_size
+        self.split = split
+
+        self.weights = self._prepare_weights(
+            reweight=reweight, lds=lds, lds_kernel=lds_kernel, lds_ks=lds_ks, lds_sigma=lds_sigma)
+
+    def __len__(self):
+        return len(self.df)
