feda4f95b017750e4c49e7b7c51999ae69c71ad7
ruizhipu <18651885620@163.com>
2022-09-28 01:02:26 -0400

    run

---
+++ slurm_jobs/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=baselines
+#SBATCH --account=def-boyuwang
+#SBATCH --time=00-08:00
+#SBATCH --mem=16G
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=6
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=18651885620@163.com
+#SBATCH --mail-type=FAIL
+#SBATCH --output=slurm_output/slurm-%j-%x.out
+
+ENVDIR= /home/ruizhipu/envs/py38
+source $ENVDIR/bin/activate
+
+python train.py --data_dir home/ruizhipu/scratch/regression/imbalanced-regression/imdb-wiki-dir/data --la False --regulize False > la_F_regu_F.txt
+
+python train.py --data_dir home/ruizhipu/scratch/regression/imbalanced-regression/imdb-wiki-dir/data --la False --regulize True > la_F_regu_T.txt
+
+python train.py --data_dir home/ruizhipu/scratch/regression/imbalanced-regression/imdb-wiki-dir/data --la True --regulize True > la_T_regu_T.txt
