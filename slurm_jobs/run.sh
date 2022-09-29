bfbce89bca636bd444a676bae454420b0a700d26
ruizhipu <18651885620@163.com>
2022-09-29 01:25:25 -0400

    refresh

--- slurm_jobs/run.sh
+++ slurm_jobs/run.sh
@@ -11,8 +11,7 @@
 #SBATCH --output=slurm_output/slurm-%j-%x.out
 module load StdEnv/2020 cuda scipy-stack python/3.8
 #
-ENVDIR= /home/ruizhipu/envs/py38
-source $ENVDIR/bin/activate
+source /home/ruizhipu/envs/py38/bin/activate
 
 echo "SLURM_JOBID: " $SLURM_JOBID
 echo "SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
