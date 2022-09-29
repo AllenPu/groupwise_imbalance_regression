692a29cf3aed7ec553d6b218d6846fbf72942f4f
ruizhipu <18651885620@163.com>
2022-09-29 01:52:50 -0400

    add

--- network.py
+++ network.py
@@ -14,6 +14,8 @@ class ResNet_regression(nn.Module):
         #
         self.model_extractor = nn.Sequential(*list(self.model.children())[:-1])
         #
+        self.Flatten = nn.Flatten(start_dim=1)
+        #
         self.model_linear =  nn.Sequential(nn.Linear(fc_inputs, output_dim))
         #
         '''
@@ -32,6 +34,8 @@ class ResNet_regression(nn.Module):
     def forward(self, x):
         #"output of model dim is 2G"
         z = self.model_extractor(x)
+        #
+        z = self.Flatten(z)
         #
         y_hat = self.model_linear(z)
         #
