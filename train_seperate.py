cef6221b4cdc36fd78a87b7ab8e9d7f54b9fa483
AllenPu <18651885620@163.com>
2022-10-03 19:14:36 -0400

    sep

--- train_seperate.py
+++ train_seperate.py
@@ -44,7 +44,7 @@ parser.add_argument('--seeds', default=123, type=int, help = ' random seed ')
 parser.add_argument('--tau', default=1, type=int, help = ' tau for logit adjustment ')
 parser.add_argument('--group_mode', default='normal', type=str, help = ' group mode for group orgnize')
 parser.add_argument('--schedule', type=int, nargs='*', default=[60, 80], help='lr schedule (when to drop lr by 10x)')
-parser.add_argument('--output_dim', type=int, nargs='*', default=0, help='output dim of network')
+parser.add_argument('--output_dim', type=int, default=0, help='output dim of network')
 
 def get_dataset(args):
     print('=====> Preparing data...')
