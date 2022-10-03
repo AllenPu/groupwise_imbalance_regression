79fd376fe6dd3babbe891a4d4fc80373fe4cf10a
AllenPu <18651885620@163.com>
2022-10-02 20:51:11 -0400

    add sep

--- network.py
+++ network.py
@@ -9,7 +9,7 @@ class ResNet_regression(nn.Module):
         self.groups = args.groups
         self.model = torchvision.models.resnet18(pretrained=False)
         #
-        output_dim = args.groups*2
+        output_dim = args.groups
         #
         fc_inputs = self.model.fc.in_features
         #
