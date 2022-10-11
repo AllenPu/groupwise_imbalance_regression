530f33c355a7eb8e40f904bfc51d5a2f2c9ddbaf
AllenPu <18651885620@163.com>
2022-10-10 21:44:43 -0400

    lr & bs

--- train_ordinary.py
+++ train_ordinary.py
@@ -113,6 +113,9 @@ def train_one_epoch(model, train_loader, ce, opt, args):
         # x shape : (batch,channel, H, W)
         # y shape : (batch, 1)
         # g hsape : (batch, 1)
+        if idx == 0:
+            print("g is ", g[:5])
+            print("o is ", o[:5])
         x, y, g, o = x.to(device), y.to(device), g.to(device), o.to(device)
         #
         y_hat,  out = model(x)
