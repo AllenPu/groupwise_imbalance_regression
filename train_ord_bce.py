55a91e3c56981146499a7a3a0fb620917d9d4221
AllenPu <18651885620@163.com>
2022-10-30 18:24:59 -0400

    add

--- train_ord_bce.py
+++ train_ord_bce.py
@@ -159,6 +159,10 @@ def test_step(model, test_loader, device):
             # write down the acc
             acc_bs = torch.sum(pred_ord == group)/bsz
             #
+            print(" test pred is ", pred_ord[:10])
+            print(" test ord is ", ord_out[:10])
+            print(" test g is ", group[:10] )
+            #
             y_predicted = torch.gather(y_output, dim = 1, index = group.to(torch.int64))
             # MSE
             mse_1 = mse(y_predicted, targets)
