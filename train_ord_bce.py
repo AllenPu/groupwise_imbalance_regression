697462ef78a25b58dac381b21c7ad8e33c13e52d
AllenPu <18651885620@163.com>
2022-10-30 18:25:10 -0400

    add

--- train_ord_bce.py
+++ train_ord_bce.py
@@ -162,6 +162,7 @@ def test_step(model, test_loader, device):
             print(" test pred is ", pred_ord[:10])
             print(" test ord is ", ord_out[:10])
             print(" test g is ", group[:10] )
+            break
             #
             y_predicted = torch.gather(y_output, dim = 1, index = group.to(torch.int64))
             # MSE
