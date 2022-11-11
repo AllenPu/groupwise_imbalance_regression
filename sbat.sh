8ff76ffa16477713aa138f01996d9a166cbca8ba
AllenPu <18651885620@163.com>
2022-11-10 21:11:50 -0500

    add

--- sbat.sh
+++ sbat.sh
@@ -1,8 +1,9 @@
 for i in 0.00003 0.00005 0.00008; do
     for j in 0.5 1; do
         for g in 10 25; do
-            jobs=${i}_${j}_${g}
-            sbatch --job-name=${jobs} run_train.sh ${i} ${j} ${g}
+            jobs='lr'+_${i}+'tau'+_${j}+'group'+_${g}
+            print(${jobs})
+            #sbatch --job-name=${jobs} run_train.sh ${i} ${j} ${g}
         done
     done
 done
