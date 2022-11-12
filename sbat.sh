6a1611bc5f25f84412eeb67100b01b1aa8ea4ae2
AllenPu <18651885620@163.com>
2022-11-12 16:59:30 -0500

    add

--- sbat.sh
+++ sbat.sh
@@ -1,7 +1,7 @@
 for i in 0.00001. 0.0000005; do
     for j in 0.5 1; do
         for g in 10 20 25 30 50; do
-            jobs='lr'+_${i}+'tau'+_${j}+'group'+_${g}
+            jobs='lr'_${i}_+'tau'_${j}_'group'_${g}
             echo ${jobs}
             sbatch --job-name=${jobs} run_train.sh ${i} ${j} ${g}
         done
