4ea7e4f18b08dc04bbd14c3cd29ed4e1f5ccbb99
AllenPu <18651885620@163.com>
2022-11-17 17:24:13 -0500

    up

--- sbat.sh
+++ sbat.sh
@@ -1,6 +1,6 @@
-for i in 0.0001 0.00001 0.000005 0.000001; do
-    for j in 0.5 1 1.5 2; do
-        for g in 10 20 25 50; do
+for i in 0.0001 0.0005 0.00005; do
+    for j in 0.5; do
+        for g in 10 25 50; do
             for d in 18 50; do
                 jobs='lr'_${i}_'tau'_${j}_'group'_${g}_'model_depth'_${d}
                 echo ${jobs}
