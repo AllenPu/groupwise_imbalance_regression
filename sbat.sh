441980674065e8f3f6dbb9eb058763a5d1aadb83
AllenPu <18651885620@163.com>
2022-11-20 17:47:19 -0500

    de

--- sbat.sh
+++ sbat.sh
@@ -1,10 +1,12 @@
-for i in 0.0001 0.0005 0.00005; do
+for i in 0.0001; do
     for j in 0.5; do
-        for g in 10 25 50; do
+        for g in 10 25; do
             for d in 18 50; do
-                jobs='lr'_${i}_'tau'_${j}_'group'_${g}_'model_depth'_${d}
-                echo ${jobs}
-                sbatch --job-name=${jobs} run_train.sh ${i} ${j} ${g} ${d}
+                for e in 120 150 170 200; do
+                    jobs='lr'_${i}_'tau'_${j}_'group'_${g}_'model_depth'_${d}_'epoch'_${e}
+                    echo ${jobs}
+                    sbatch --job-name=${jobs} run_train.sh ${i} ${j} ${g} ${d} ${e}
+                done
             done
         done
     done
