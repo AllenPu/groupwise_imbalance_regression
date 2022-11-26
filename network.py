fb3f89453a629dd678dc8ee417a4de791aabff84
AllenPu <18651885620@163.com>
2022-11-26 18:55:23 -0500

    add

--- network.py
+++ network.py
@@ -157,3 +157,36 @@ class ResNet_regression_split(nn.Module):
         # the ouput dim of the embed is : 512
         #
         return y_hat, g_hat
+
+class ResNet_regression_ddp(nn.Module):
+    def __init__(self, args):
+        super(ResNet_regression_ddp, self).__init__()
+        self.groups = args.groups
+        exec('self.model = torchvision.models.resnet{}(pretrained=False)'.format(args.model_depth))
+        #
+        output_dim = args.groups * 2
+        #
+        fc_inputs = self.model.fc.in_features
+        #
+        self.model_extractor = nn.Sequential(*list(self.model.children())[:-1])
+        #
+        self.Flatten = nn.Flatten(start_dim=1)
+        #
+        self.model_linear =  nn.Sequential(nn.Linear(fc_inputs, output_dim))
+        #
+
+        #self.mode = args.mode
+        self.sigma = args.sigma
+        
+    # g is the same shape of y
+    def forward(self, x):
+        #"output of model dim is 2G"
+        z = self.model_extractor(x)
+        #
+        z = self.Flatten(z)
+        #
+        y_hat = self.model_linear(z)
+        #
+        # the ouput dim of the embed is : 512
+        #
+        return y_hat
