4459dd1923aa20ff4ebbf417e45f323af3188d9d
AllenPu <18651885620@163.com>
2022-11-29 20:45:22 -0500

    add loss bmc

--- loss.py
+++ loss.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 import numpy as np
+from torch.nn.modules.loss import _Loss
 
 class LAloss(nn.Module):
     def __init__(self, cls_num_list, tau=1.0):
@@ -39,6 +40,25 @@ class Weight_CE(nn.Module):
         #loss = torch.sum( -y * torch.log(x))
         return loss
 
+
+
+class BMCLoss(_Loss):
+    def __init__(self, init_noise_sigma):
+        super(BMCLoss, self).__init__()
+        self.noise_sigma = torch.nn.Parameter(torch.tensor(init_noise_sigma, device="cuda"))
+
+    def forward(self, pred, target):
+        noise_var = self.noise_sigma ** 2
+        loss = bmc_loss(pred, target, noise_var)
+        return loss
+
+
+def bmc_loss(pred, target, noise_var):
+    logits = - 0.5 * (pred - target.T).pow(2) / noise_var
+    loss = F.cross_entropy(logits, torch.arange(pred.shape[0]).cuda())
+    loss = loss * (2 * noise_var).detach()
+
+    return loss
 
 '''
     This is the forward but not the loss 
