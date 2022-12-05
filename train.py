647473e114e56932240426fa18c8e7a761d788c8
AllenPu <18651885620@163.com>
2022-12-04 21:08:32 -0500

    add l1 to he training as tau_dis

--- train.py
+++ train.py
@@ -271,8 +271,9 @@ if __name__ == '__main__':
     #
     total_result = 'total_result_model_'+str(args.model_depth)+'.txt'
     #
-    store_name = 'la_' + str(args.la) + '_regu_' + str(args.regulize) + '_tau_'+ str(args.tau) + \
-                        '_lr_' + str(args.lr) + '_g_'+ str(args.groups) + '_model_' + str(args.model_depth) + '_epoch_' + str(args.epoch)
+    store_name = 'la_' + str(args.la)  + '_tau_'+ str(args.tau) + \
+                        '_lr_' + str(args.lr) + '_g_'+ str(args.groups) + '_model_' + str(args.model_depth) + \
+                        '_epoch_' + str(args.epoch) + '_group_dis_' + str(args.tau_dis)
     ####
     print(" store name is ", store_name)
     #
@@ -300,7 +301,7 @@ if __name__ == '__main__':
         model = train_one_epoch(model, train_loader, loss_ce, loss_mse, opt, args)
         if e%20 == 0:
             cls_acc, reg_mae = validate(model, val_loader, train_labels)
-            with open(store_name, 'a') as f:
+            with open(store_name, 'a+') as f:
                 f.write(' In epoch {} cls acc is {} regression mae is {}'.format(e, cls_acc, reg_mae) + '\n')
                 #f.write(' tolerance is {}'.format())
                 f.close()
@@ -310,7 +311,7 @@ if __name__ == '__main__':
     #
     print(' mse of gt is {}, mse of pred is {}, acc of the group assinment is {}, \
             mae of gt is {}, mae of pred is {}'.format(acc_gt, acc_pred, g_pred, mae_gt, mae_pred))
-    with open(store_name, 'a') as f:
+    with open(store_name, 'a+') as f:
         f.write(' tau is {} group is {} lr is {} model depth {} epoch {}'.format(args.tau, args.groups, args.lr, args.model_depth, args.epoch) +"\n" )
         f.write(' mse of gt is {}, mse of pred is {}, acc of the group assinment is {}, \
             mae of gt is {}, mae of pred is {}'.format(acc_gt, acc_pred, g_pred, mae_gt, mae_pred)+"\n")
