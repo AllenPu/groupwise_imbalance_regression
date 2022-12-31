75afc95abb6d7db7198f50f00ac708cfd05becf0
AllenPu <18651885620@163.com>
2022-12-30 20:54:13 -0500

    add agedb

---
+++ agedb/train_age_db.py
@@ -0,0 +1,59 @@
+import time
+import argparse
+import logging
+from tqdm import tqdm
+import pandas as pd
+from collections import defaultdict
+from scipy.stats import gmean
+from utils import *
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.utils.data import DataLoader
+from datasets import AgeDB
+
+
+import os
+os.environ["KMP_WARNINGS"] = "FALSE"
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+# training/optimization related
+parser.add_argument('--dataset', type=str, default='agedb', choices=['imdb_wiki', 'agedb'], help='dataset name')
+parser.add_argument('--data_dir', type=str, default='./data', help='data directory')
+parser.add_argument('--model', type=str, default='resnet50', help='model name')
+parser.add_argument('--store_root', type=str, default='checkpoint', help='root path for storing checkpoints, logs')
+parser.add_argument('--store_name', type=str, default='', help='experiment store name')
+parser.add_argument('--gpu', type=int, default=None)
+parser.add_argument('--optimizer', type=str, default='adam', choices=['adam', 'sgd'], help='optimizer type')
+parser.add_argument('--loss', type=str, default='l1', choices=['mse', 'l1', 'focal_l1', 'focal_mse', 'huber'], help='training loss type')
+parser.add_argument('--lr', type=float, default=1e-3, help='initial learning rate')
+parser.add_argument('--epoch', type=int, default=90, help='number of epochs to train')
+parser.add_argument('--momentum', type=float, default=0.9, help='optimizer momentum')
+parser.add_argument('--weight_decay', type=float, default=1e-4, help='optimizer weight decay')
+parser.add_argument('--schedule', type=int, nargs='*', default=[60, 80], help='lr schedule (when to drop lr by 10x)')
+parser.add_argument('--batch_size', type=int, default=256, help='batch size')
+parser.add_argument('--print_freq', type=int, default=10, help='logging frequency')
+parser.add_argument('--img_size', type=int, default=224, help='image size used in training')
+parser.add_argument('--workers', type=int, default=32, help='number of workers used in data loading')
+
+
+
+
+def get_data_loader(args):
+    print('=====> Preparing data...')
+    df = pd.read_csv(os.path.join(args.data_dir, "agedb.csv"))
+    df_train, df_val, df_test = df[df['split'] == 'train'], df[df['split'] == 'val'], df[df['split'] == 'test']
+    train_labels = df_train['age']
+    #
+    train_dataset = AgeDB(data_dir=args.data_dir, df=df_train, img_size=args.img_size, split='train',reweight=args.reweight)
+    val_dataset = AgeDB(data_dir=args.data_dir, df=df_val, img_size=args.img_size, split='val')
+    test_dataset = AgeDB(data_dir=args.data_dir, df=df_test, img_size=args.img_size, split='test')
+    #
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
+                              num_workers=args.workers, pin_memory=True, drop_last=False)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
+                            num_workers=args.workers, pin_memory=True, drop_last=False)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
+                             num_workers=args.workers, pin_memory=True, drop_last=False)
+    print(f"Training data size: {len(train_dataset)}")
+    print(f"Validation data size: {len(val_dataset)}")
+    print(f"Test data size: {len(test_dataset)}")
+    return train_loader, val_loader, test_loader, train_labels
