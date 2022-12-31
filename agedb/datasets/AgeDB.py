9d68cc3d42ed9477001688b498ad25b53bf8cc3e
AllenPu <18651885620@163.com>
2022-12-30 20:54:41 -0500

    add agedb

--- agedb/datasets/AgeDB.py
+++ agedb/datasets/AgeDB.py
@@ -5,11 +5,12 @@ from PIL import Image
 from scipy.ndimage import convolve1d
 from torch.utils import data
 import torchvision.transforms as transforms
+import pandas as pd
+from torch.utils.data import DataLoader
 
 
 class AgeDB(data.Dataset):
-    def __init__(self, df, data_dir, img_size, split='train', reweight='none',
-                 lds=False, lds_kernel='gaussian', lds_ks=5, lds_sigma=2):
+    def __init__(self, df, data_dir, img_size, split='train', reweight='none'):
         self.df = df
         self.data_dir = data_dir
         self.img_size = img_size
@@ -42,3 +43,33 @@ class AgeDB(data.Dataset):
                 transforms.Normalize([.5, .5, .5], [.5, .5, .5]),
             ])
         return transform
+
+    def _prepare_weights(self, reweight, max_target=121, lds=False, lds_kernel='gaussian', lds_ks=5, lds_sigma=2):
+        assert reweight in {'none', 'inverse', 'sqrt_inv'}
+        assert reweight != 'none' if lds else True, \
+            "Set reweight to \'sqrt_inv\' (default) or \'inverse\' when using LDS"
+
+        value_dict = {x: 0 for x in range(max_target)}
+        labels = self.df['age'].values
+        for label in labels:
+            value_dict[min(max_target - 1, int(label))] += 1
+        if reweight == 'sqrt_inv':
+            value_dict = {k: np.sqrt(v) for k, v in value_dict.items()}
+        elif reweight == 'inverse':
+            value_dict = {k: np.clip(v, 5, 1000) for k, v in value_dict.items()}  # clip weights for inverse re-weight
+        num_per_label = [value_dict[min(max_target - 1, int(label))] for label in labels]
+        if not len(num_per_label) or reweight == 'none':
+            return None
+        print(f"Using re-weighting: [{reweight.upper()}]")
+
+        if lds:
+            lds_kernel_window = get_lds_kernel_window(lds_kernel, lds_ks, lds_sigma)
+            print(f'Using LDS: [{lds_kernel.upper()}] ({lds_ks}/{lds_sigma})')
+            smoothed_value = convolve1d(
+                np.asarray([v for _, v in value_dict.items()]), weights=lds_kernel_window, mode='constant')
+            num_per_label = [smoothed_value[min(max_target - 1, int(label))] for label in labels]
+
+        weights = [np.float32(1 / x) for x in num_per_label]
+        scaling = len(weights) / np.sum(weights)
+        weights = [scaling * x for x in weights]
+        return weights
