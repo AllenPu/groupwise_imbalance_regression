79bd6f5cb5fb8374476874d582eb850ca04e2f65
ruizhipu <18651885620@163.com>
2023-01-08 00:59:16 -0500

    debug

--- agedb/train_age_db.py
+++ agedb/train_age_db.py
@@ -69,7 +69,7 @@ def get_data_loader(args):
     df_train, df_val, df_test = df[df['split'] == 'train'], df[df['split'] == 'val'], df[df['split'] == 'test']
     train_labels = df_train['age']
     #
-    train_dataset = AgeDB(data_dir=args.data_dir, df=df_train, img_size=args.img_size, split='train',reweight=args.reweight)
+    train_dataset = AgeDB(data_dir=args.data_dir, df=df_train, img_size=args.img_size, split='train',reweight=args.reweight, group_num=args.groups)
     #
     group_list = train_dataset.get_group_list()
     #
